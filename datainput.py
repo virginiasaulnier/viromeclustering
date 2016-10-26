0505db9f5c3d200542960c9aa6ad6ae47029b225
virginiasaulnier <vsaulnier@luc.edu>
2016-10-26 12:56:43 -0500

    added debug lines, to find delays in loops

--- datainput.py
+++ datainput.py
@@ -1,8 +1,10 @@
 from Bio import SeqIO
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
-
-file="/Users/Virginiasaulnier/Downloads/v1S2.consensus"
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('This is a log message.')
+file="/Users/Virginiasaulnier/Downloads/v1S2short.consensus"
 
 largecontigs= (rec for rec in SeqIO.parse(file,"fasta") if len(rec)>2000)
 SeqIO.write(largecontigs, "largecontigs.fasta", "fasta")
@@ -34,36 +36,33 @@ for seq_record in SeqIO.parse("largecontigs.fasta", "fasta"):
     #fasta_string = open(i+"/Users/Virginiasaulnier/Downloads/largecontigs.fasta").read() #or make the names fasta1.fasta and just do open(i).read
     result_handle = NCBIWWW.qblast("blastn","nt", seq_record.seq, entrez_query="bacteria[organism]", expect=.0001)
     blast_records = NCBIXML.parse(result_handle)
-
+    logging.debug('This is a log message after parse blast_records.')
 
     for record in blast_records:
         bacteriacontigs.append(seq_record.id)
-    print('in seq record')
+        logging.debug('This is a log message appending bacteria seq_records.')
 print(bacteriacontigs)
 
 #print(bacteriacontigs)
 
 for record in blast_records:
     if seq_record.id in bacteriacontigs:
-         result_handle_virus = NCBIWWW.qblast("blastn", "nt", seq_record.seq, entrez_query="viruses[organism] AND 1:30[perc_ident]",
-                                           expect=.0001)
+         logging.debug('This is a log message in virus blast.')
+         result_handle_virus = NCBIWWW.qblast("blastn", "nt", seq_record.seq, entrez_query="viruses[organism]")
          blast_records_virus = NCBIXML.parse(result_handle_virus)
 
          for record in blast_records_virus:
             viralcontigs.append(seq_record.id)
-         print('in virus blast')
 print(viralcontigs)
 
 
 
 for record in blast_records:
     if seq_record.id in bacteriacontigs:
-        result_handle_viralprotein=NCBIWWW.qblast("blastx", "pdb", seq_record.seq, entrez_query="viruses[organism] AND 1:30[perc_ident]",
-                                           expect=.0001)
-        blast_records_viralprotein = NCBIXML.parse(result_handle_viralprotein)
+        logging.debug('This is a log message in viral protein blast.')
+        result_handle_viralprotein=NCBIWWW.qblast("blastx", "pdb", seq_record.seq, entrez_query="viruses[organism]")
 
         for record in blast_records_viralprotein:
             viralcontigs.append(seq_record.id)
-        print('in viral protein blast')
 print(viralcontigs)
 
