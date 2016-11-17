788fef6476e11a1e12d730338fb29bdc1c1b2498
virginiasaulnier <vsaulnier@luc.edu>
2016-11-16 23:22:57 -0600

    getting clustering sequences in file

--- datainput.py
+++ datainput.py
@@ -4,17 +4,11 @@ from Bio.Blast import NCBIXML
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug('This is a log message.')
-file="/Users/Virginiasaulnier/Downloads/v1S2short.consensus"
+file="/Users/Virginiasaulnier/Downloads/v1S2tiny.consensus"
 
 largecontigs= (rec for rec in SeqIO.parse(file,"fasta") if len(rec)>2000)
 SeqIO.write(largecontigs, "largecontigs.fasta", "fasta")
-#for seq_record in SeqIO.parse("largecontigs.fasta", "fasta"):
-    # print(seq_record.id)
-    # print(repr(seq_record.seq))
-    # print(len(seq_record))
 
-
-#x = 1
 
 bacteriacontigs = []
 def add(i, s):
@@ -31,9 +25,14 @@ def add(i, s):
         viralcontigs[i] = s
 
 
+clustercontigs = []
+def add(i, s):
+    size = len(viralcontigs)
+    if i >= size:
+        viralcontigs.extend([None]*(i-size+1))
+        viralcontigs[i] = s
+
 for seq_record in SeqIO.parse("largecontigs.fasta", "fasta"):
-    #File = open("/Users/Virginiasaulnier/Downloads/results" + str(x) + ".txt", "w")
-    #fasta_string = open(i+"/Users/Virginiasaulnier/Downloads/largecontigs.fasta").read() #or make the names fasta1.fasta and just do open(i).read
     result_handle = NCBIWWW.qblast("blastn","nt", seq_record.seq, entrez_query="bacteria[organism]", expect=.0001)
     blast_records = NCBIXML.parse(result_handle)
     logging.debug('This is a log message after parse blast_records.')
@@ -82,3 +81,14 @@ for seq_record in SeqIO.parse("largecontigs.fasta", "fasta"):
 
 print(viralcontigs)
 
+clusterids=set(bacteriacontigs) - set(viralcontigs)
+
+print(clusterids)
+
+for seq_record in SeqIO.parse("largecontigs.fasta", "fasta"):
+    if seq_record.id in bacteriacontigs:
+        while(i==0):
+            clustercontigs.append(seq_record)
+            i=i+1
+
+print(clustercontigs)
